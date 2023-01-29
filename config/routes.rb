dcda0cad5a07cdf11231ad67ed3b30fe833cff15
Do Minh Hieu <hieudo030801@gmail.com>
2023-01-29 17:25:58 +0000

    Add profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/show'
   root to: 'static_pages#home'
   devise_scope :user do  
     get '/users/sign_out' => 'devise/sessions#destroy'     
   end
   devise_for :users
+  get 'users/show'
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
